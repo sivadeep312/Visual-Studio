bfe7ef2ca31743d2bcf5bdd5bfc2495f0f0b8229
sivawebdesign <64145649+sivawebdesign@users.noreply.github.com>
2022-05-17 01:03:25 +0530

    Add files via upload

--- cir_dec.cpp
+++ cir_dec.cpp
@@ -1,3 +1,4 @@
+
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
@@ -8,8 +9,7 @@ using namespace cv;
 int main()
 {
 
-	string image_path1 = "Res/Input.png";
-	Mat img = imread(image_path1, IMREAD_COLOR);
+	Mat img = imread("C:/Users/sivadeep.p/source/repos/images/Input.png", IMREAD_COLOR);
 	resize(img, img, { 500,500 });
 	Mat F = img.clone();
 	Mat G = img.clone();
@@ -20,7 +20,7 @@ int main()
 		cout << "Could not read the image: " << endl;
 		return 1;
 	}
-	Mat imgGray, imgBlur, imgCanny, imgDil, imgErode,difgray, imgThreshold, binaryImg;
+	Mat imgGray, imgBlur, imgCanny, imgDil, imgErode, difgray, imgThreshold, binaryImg;
 	cvtColor(img, imgGray, COLOR_BGR2GRAY);
 	GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
 	Canny(imgBlur, imgCanny, 25, 75);
@@ -30,16 +30,14 @@ int main()
 	cvtColor(img, gray, COLOR_BGR2GRAY);
 
 	vector<Vec3f> circles;
-	HoughCircles(gray, circles, HOUGH_GRADIENT, 1, gray.rows / 10,100, 30, 40, 55);
+	HoughCircles(gray, circles, HOUGH_GRADIENT, 1, gray.rows / 10, 100, 30, 40, 55);
 	for (size_t i = 0; i < circles.size(); i++)
 	{
 		Vec3i c = circles[i];
 		Point center = Point(c[0], c[1]);
-		cout << "center is" << center;
 		int radius = c[2];
 		circle(F, center, radius, Scalar(0, 0, 0), FILLED);
 	}
-	
 	Mat diff = img - F;
 	//Mat add = newimg + diff;
 	imshow("Orginal Image", diff);
@@ -59,9 +57,11 @@ int main()
 		drawContours(G, contours, contour, colour, FILLED, 8, hierarchy);
 
 	}
-	
+
+
+
 	Mat ng = diff - G;
-	imshow("Yellow ", ng);
+	imshow("Op", ng);
 	
 
 	int k = waitKey(0); // Wait for a keystroke in the window
